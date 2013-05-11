e5e34daf6237ce5455406ac6ca647492b7a359fe
Endain <endain.gaming@gmail.com>
2013-05-10 21:30:40 -0700

    Added WorldGuard, WorldEdit, Vault and Lombok as dependencies. Added cell rental skeleton files.

---
+++ src/main/java/net/conriot/prison/cells/Cell.java
@@ -0,0 +1,9 @@
+package net.conriot.prison.cells;
+
+public class Cell
+{
+	public static void main(String[] args)
+	{
+		//TODO @Endain
+	}
+}
---
+++ src/main/java/net/conriot/prison/cells/CellManager.java
@@ -0,0 +1,16 @@
+package net.conriot.prison.cells;
+
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+
+import net.conriot.prison.ConRiot;
+
+public class CellManager
+{
+	private ConRiot plugin;
+	private WorldGuardPlugin wg;
+	
+	public static void main(String[] args)
+	{
+		// TODO @Endain
+	}
+}
